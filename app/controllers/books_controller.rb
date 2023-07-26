d25b49f3e9840d001a877d11fcfed4233a051301
EC2 Default User <ec2-user@ip-172-31-33-83.ap-northeast-1.compute.internal>
2023-07-26 08:58:29 +0000

    Initial Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -33,12 +33,12 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     if @book.update(book_params)
       flash[:notice] = "successfully"
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
       render :edit
     end
   end
-def destroy
+  def destroy
   book = Book.find(params[:id])  # データ（レコード）を1件取得
     book.destroy  # データ（レコード）を削除
     redirect_to '/books'  # 投稿一覧画面へリダイレクト
